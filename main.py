c9ff5b37a11ef5f24ef1f9d8eb53bbcffe6e10a7
anthgg <cryptoanthdm@gmail.com>
2024-12-13 11:38:18 +0100

    renamed

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import runpod
 
 def is_even(job):
     job_input = job["input"]
-    job_id = job_input["job_id"]
-    job_metadata = job_input["metadata"]
+    job_id = job_input["jobId"]
+    job_metadata = job_input.get("metadata")
 
     prompt = job_input["prompt"]
     if prompt == "a resource that fails":
@@ -19,7 +19,6 @@ def is_even(job):
     return {
         "resource_url": "https://delivery.fanmade.ai/05_b2dd4b5c-c714-4e30-b980-01045e472fb1.png",
         "duration": 4.1,
-        "job_id": job_id,
         "status": "completed",
         "metadata": job_metadata,
     }
