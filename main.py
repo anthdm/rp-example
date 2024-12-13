c7c2e917351d5a50f56f5c47f9fb787d7914f551
anthgg <cryptoanthdm@gmail.com>
2024-12-13 12:50:37 +0100

    input output

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ import runpod
 
 def is_even(job):
     job_input = job["input"]
+    model_input = job_input.get("modelInput")
 
-    prompt = job_input["prompt"]
+    prompt = model_input["prompt"]
     if prompt == "a resource that fails":
         return {"error": "you have entered (a resource that fails)"}
 
     return {
-        "resource_url": "https://delivery.fanmade.ai/05_b2dd4b5c-c714-4e30-b980-01045e472fb1.png",
+        "output": "https://delivery.fanmade.ai/05_b2dd4b5c-c714-4e30-b980-01045e472fb1.png",
         "nsfw": False,
     }
 
