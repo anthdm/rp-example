73693176fd9df86fce9fd1ddfb49e17b56cd0714
anthgg <cryptoanthdm@gmail.com>
2024-12-13 11:48:16 +0100

    job_id

--- main.py
+++ main.py
@@ -7,12 +7,7 @@ def is_even(job):
 
     prompt = job_input["prompt"]
     if prompt == "a resource that fails":
-        return {
-            "status": "failed",
-            "duration": 1.8,
-            "error_message": "you have entered (a resource that fails)",
-            "metadata": job_metadata,
-        }
+        return {"error": "you have entered (a resource that fails)"}
 
     return {
         "resource_url": "https://delivery.fanmade.ai/05_b2dd4b5c-c714-4e30-b980-01045e472fb1.png",
